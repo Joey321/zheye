4ba8d2dacd27aeec9e350e04000b59cd473cd309
shiojiang <shiojiang@tencent.com>
2022-06-23 10:19:31 +0800

    完成专栏ColumnList组件的开发

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <ColumnList :list="testData"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_150,w_150',
+    title: 'test title',
+    description: 'test description'
+  },
+  {
+    id: 2,
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_150,w_150',
+    title: 'test title2',
+    description: 'test description2'
+  },
+  {
+    id: 3,
+    title: 'test title3',
+    description: 'test description3'
+  },
+  {
+    id: 4,
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_150,w_150',
+    title: 'test title4',
+    description: 'test description4'
+  }
+]
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ColumnList
+  },
+  setup () {
+    return {
+      testData
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="row">
+    <div v-for="column in columnList" :key="column.id" class="col-4 mb-3">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img :src="column.avatar" :alt="column.title" class="rounded-circle border border-light w-25 my-3" />
+          <h5>{{column.title}}</h5>
+          <p>{{column.description}}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, computed } from 'vue'
+export interface ColumnProps {
+  id: number,
+  avatar?: string,
+  title: string,
+  description: string
+}
+
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true
+    }
+  },
+  setup (props) {
+    // 重新计算 为无avatar的项添加默认avatar
+    const columnList = computed(() => {
+      return props.list.map(column => {
+        if (!column.avatar) {
+          column.avatar = require('../assets/avatar.jpg')
+        }
+        return column
+      })
+    })
+    return {
+      columnList
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
